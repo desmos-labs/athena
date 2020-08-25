ed544f18e97160e5eaebd52838214a0da6fabffb
riccardo.montagnin <riccardo.montagnin@gmail.com>
2020-08-25 07:58:21 +0200

    Updated how data are read from genesis

--- x/posts/genesis_handler.go
+++ x/posts/genesis_handler.go
@@ -29,7 +29,7 @@ func GenesisHandler(
 	genPosts := genState.Posts
 	sort.SliceStable(genPosts, func(i, j int) bool {
 		first, second := genPosts[i], genPosts[j]
-		return second.Created.Before(first.Created)
+		return first.Created.Before(second.Created)
 	})
 
 	// Save the poststypes
