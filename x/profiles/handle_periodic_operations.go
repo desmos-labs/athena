8f186850e5a41572b26f0235a64b8922a3988689
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2021-11-15 09:36:52 +0100

    fix: updated logging value for profiles params

--- x/profiles/handle_periodic_operations.go
+++ x/profiles/handle_periodic_operations.go
@@ -9,7 +9,7 @@ import (
 
 // RegisterPeriodicOperations implements modules.PeriodicOperationsModule
 func (m *Module) RegisterPeriodicOperations(scheduler *gocron.Scheduler) error {
-	log.Debug().Str("module", "profiles").Msg("setting up periodic tasks")
+	log.Info().Str("module", "profiles").Msg("setting up periodic tasks")
 
 	// Update the params every 30 mins
 	if _, err := scheduler.Every(30).Minutes().StartImmediately().Do(m.updateParams); err != nil {
