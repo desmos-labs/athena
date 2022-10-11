90dd17ed1fa84864765a435d98e3022c1d008b56
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2022-10-11 11:24:58 -0500

    feat: added the ability to parse smart contracts data

---
+++ cmd/parse/contracts/cmd.go
@@ -0,0 +1,20 @@
+package profiles
+
+import (
+	parsecmdtypes "github.com/forbole/juno/v3/cmd/parse/types"
+	"github.com/spf13/cobra"
+)
+
+// NewContractsCmd returns the Cobra command that allows to parse things related to smart contracts
+func NewContractsCmd(parseCfg *parsecmdtypes.Config) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "contracts",
+		Short: "Parse things related to smart contracts",
+	}
+
+	cmd.AddCommand(
+		contractsCmd(parseCfg),
+	)
+
+	return cmd
+}
---
+++ cmd/parse/contracts/contracts.go
@@ -0,0 +1,64 @@
+package profiles
+
+import (
+	"fmt"
+
+	subspacestypes "github.com/desmos-labs/desmos/v4/x/subspaces/types"
+	parsecmdtypes "github.com/forbole/juno/v3/cmd/parse/types"
+	"github.com/forbole/juno/v3/node/remote"
+	"github.com/forbole/juno/v3/types/config"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
+
+	"github.com/desmos-labs/djuno/v2/database"
+	"github.com/desmos-labs/djuno/v2/x/contracts"
+	"github.com/desmos-labs/djuno/v2/x/contracts/tips"
+)
+
+// contractsCmd returns a Cobra command that allows to refresh the smart contracts data for a single subspace
+func contractsCmd(parseConfig *parsecmdtypes.Config) *cobra.Command {
+	return &cobra.Command{
+		Use:   "all [subspace-id]",
+		Args:  cobra.ExactArgs(1),
+		Short: "Refresh all the smart contracts data related to the given subspace",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			subspaceID, err := subspacestypes.ParseSubspaceID(args[0])
+			if err != nil {
+				return err
+			}
+
+			parseCtx, err := parsecmdtypes.GetParserContext(config.Cfg, parseConfig)
+			if err != nil {
+				return err
+			}
+
+			remoteCfg, ok := config.Cfg.Node.Details.(*remote.Details)
+			if !ok {
+				panic(fmt.Errorf("cannot run DJuno on local node"))
+			}
+
+			// Get the database
+			db := database.Cast(parseCtx.Database)
+
+			grpcConnection := remote.MustCreateGrpcConnection(remoteCfg.GRPC)
+			contractsModule := contracts.NewModule([]contracts.SmartContractModule{
+				tips.NewModule(config.Cfg, parseCtx.Node, grpcConnection, db),
+			})
+
+			// Get the latest height
+			height, err := parseCtx.Node.LatestHeight()
+			if err != nil {
+				return err
+			}
+
+			// Refresh the smart contracts data
+			log.Info().Int64("height", height).Uint64("subspace id", subspaceID).Msg("refreshing contracts")
+			err = contractsModule.RefreshData(height, subspaceID)
+			if err != nil {
+				return err
+			}
+
+			return nil
+		},
+	}
+}
