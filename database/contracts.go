92abc1875af6891b95e38b52897a71b1a424be33
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2022-10-26 11:35:46 -0500

    fix: fixed wrong argument order while storing contract info

--- database/contracts.go
+++ database/contracts.go
@@ -8,7 +8,7 @@ import (
 func (db *Db) SaveContract(contract types.Contract) error {
 	stmt := `
 INSERT INTO contract (address, type, config, height)
-VALUES ($1, $2, $4, $3)
+VALUES ($1, $2, $3, $4)
 ON CONFLICT (address) DO UPDATE 
     SET address = excluded.address, 
         type = excluded.type,
