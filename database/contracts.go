414b3ba2662f4aeb70b6ce04e845a77cdf4c8c44
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2022-10-26 11:30:49 -0500

    fix: fixed contract config not being stored properly

--- database/contracts.go
+++ database/contracts.go
@@ -7,13 +7,14 @@ import (
 // SaveContract stores the given contract data into the database
 func (db *Db) SaveContract(contract types.Contract) error {
 	stmt := `
-INSERT INTO contract (address, type, height)
-VALUES ($1, $2, $3)
+INSERT INTO contract (address, type, config, height)
+VALUES ($1, $2, $4, $3)
 ON CONFLICT (address) DO UPDATE 
     SET address = excluded.address, 
-        type = excluded.type, 
+        type = excluded.type,
+        config = excluded.config,
         height = excluded.height
 WHERE contract.height <= excluded.height`
-	_, err := db.SQL.Exec(stmt, contract.Address, contract.Type, contract.Height)
+	_, err := db.SQL.Exec(stmt, contract.Address, contract.Type, string(contract.ConfigBz), contract.Height)
 	return err
 }
