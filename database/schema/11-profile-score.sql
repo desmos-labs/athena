facdbdbdb90567e68fd904ed2f11bdde60f83f94
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2022-11-03 16:17:15 -0600

    fix: removed double database schema

--- database/schema/11-profile-score.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE application_link_score
-(
-    id                      SERIAL                      NOT NULL,
-    application_link_row_id BIGINT                      NOT NULL REFERENCES application_link (id) ON DELETE CASCADE UNIQUE,
-    details                 JSONB                       NOT NULL,
-    score                   INT                         NOT NULL DEFAULT 0,
-    timestamp               TIMESTAMP WITHOUT TIME ZONE NOT NULL
-);
