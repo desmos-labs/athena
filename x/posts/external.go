e65b2c943bcefd3e75988cb5f612fee8400ee709
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2023-01-12 16:58:07 -0500

    feat: expose useful methods from posts module

--- x/posts/external.go
+++ x/posts/external.go
@@ -22,7 +22,7 @@ import (
 
 // RefreshPostsData refreshes all the posts' data for the given subspace
 func (m *Module) RefreshPostsData(height int64, subspaceID uint64) error {
-	postsTxs, err := m.queryPostsTxs(height, subspaceID)
+	postsTxs, err := m.QueryPostsTxs(height, subspaceID)
 	if err != nil {
 		return err
 	}
@@ -39,53 +39,65 @@ func (m *Module) RefreshPostsData(height int64, subspaceID uint64) error {
 
 	// Refresh posts
 	for _, post := range posts {
-		log.Debug().Uint64("subspace", post.SubspaceID).Uint64("post", post.ID).Msg("refreshing post")
+		// Get the post transactions
+		postTxs := m.GetPostTxHashes(postsTxs, post)
 
-		err = m.db.SavePost(post)
-		if err != nil {
-			return fmt.Errorf("error while saving post: %s", err)
-		}
-
-		postTxs := m.getPostTxHashes(postsTxs, post)
-
-		// Refresh transactions
-		for _, tx := range postTxs {
-			err = m.db.SavePostTx(tx)
-			if err != nil {
-				return fmt.Errorf("error while saving post transaction: %s", tx.Hash)
-			}
-		}
-
-		attachments, err := m.queryPostAttachments(height, post.SubspaceID, post.ID)
+		// Refresh the post data
+		err = m.RefreshPostData(height, postTxs, post)
 		if err != nil {
 			return err
 		}
+	}
 
-		// Refresh attachments
-		for _, attachment := range attachments {
+	return nil
+}
+
+// RefreshPostData refreshes the data associated with the given post at the specified height
+func (m *Module) RefreshPostData(height int64, postTxs []types.PostTransaction, post types.Post) error {
+	log.Debug().Uint64("subspace", post.SubspaceID).Uint64("post", post.ID).Msg("refreshing post")
+
+	err := m.db.SavePost(post)
+	if err != nil {
+		return fmt.Errorf("error while saving post: %s", err)
+	}
+
+	// Refresh transactions
+	for _, tx := range postTxs {
+		err = m.db.SavePostTx(tx)
+		if err != nil {
+			return fmt.Errorf("error while saving post transaction: %s", tx.Hash)
+		}
+	}
+
+	attachments, err := m.QueryPostAttachments(height, post.SubspaceID, post.ID)
+	if err != nil {
+		return err
+	}
+
+	// Refresh attachments
+	for _, attachment := range attachments {
+		log.Debug().Uint64("subspace", attachment.SubspaceID).Uint64("post", attachment.PostID).
+			Uint32("attachment", attachment.ID).Msg("refreshing attachment")
+
+		err = m.db.SavePostAttachment(attachment)
+		if err != nil {
+			return fmt.Errorf("error while saving post attachment: %s", err)
+		}
+
+		// Refresh poll answers
+		if _, isPoll := attachment.Content.GetCachedValue().(*poststypes.Poll); isPoll {
 			log.Debug().Uint64("subspace", attachment.SubspaceID).Uint64("post", attachment.PostID).
-				Uint32("attachment", attachment.ID).Msg("refreshing attachment")
+				Uint32("poll", attachment.ID).Msg("refreshing poll answers")
 
-			err = m.db.SavePostAttachment(attachment)
+			answers, err := m.queryPollAnswers(height, attachment.SubspaceID, attachment.PostID, attachment.ID)
 			if err != nil {
-				return fmt.Errorf("error while saving post attachment: %s", err)
+				return err
 			}
 
-			// Refresh poll answers
-			if _, isPoll := attachment.Content.GetCachedValue().(*poststypes.Poll); isPoll {
-				log.Debug().Uint64("subspace", attachment.SubspaceID).Uint64("post", attachment.PostID).
-					Uint32("poll", attachment.ID).Msg("refreshing poll answers")
-
-				answers, err := m.queryPollAnswers(height, attachment.SubspaceID, attachment.PostID, attachment.ID)
+			for _, answer := range answers {
+				err = m.db.SavePollAnswer(answer)
 				if err != nil {
-					return err
-				}
-
-				for _, answer := range answers {
-					err = m.db.SavePollAnswer(answer)
-					if err != nil {
-						return fmt.Errorf("errror while saving poll answer: %s", err)
-					}
+					return fmt.Errorf("errror while saving poll answer: %s", err)
 				}
 			}
 		}
@@ -125,8 +137,8 @@ func (m *Module) QuerySubspacePosts(height int64, subspaceID uint64) ([]types.Po
 	return posts, nil
 }
 
-// queryPostsTxs queries all the posts transactions made inside the given subspace
-func (m *Module) queryPostsTxs(height int64, subspaceID uint64) ([]*coretypes.ResultTx, error) {
+// QueryPostsTxs queries all the posts transactions made inside the given subspace
+func (m *Module) QueryPostsTxs(height int64, subspaceID uint64) ([]*coretypes.ResultTx, error) {
 	queries := []string{
 		// MsgCreatePost
 		fmt.Sprintf("%s.%s='%d' AND tx.height <= %d",
@@ -175,8 +187,8 @@ func (m *Module) queryPostsTxs(height int64, subspaceID uint64) ([]*coretypes.Re
 	return txs, nil
 }
 
-// filters the given transactions and returns only the ones related to the given post
-func (m *Module) getPostTxHashes(txs []*coretypes.ResultTx, post types.Post) []types.PostTransaction {
+// GetPostTxHashes filters the given transactions and returns only the ones related to the given post
+func (m *Module) GetPostTxHashes(txs []*coretypes.ResultTx, post types.Post) []types.PostTransaction {
 	var postTxs []types.PostTransaction
 	for _, tx := range txs {
 		for _, event := range tx.TxResult.Events {
@@ -192,8 +204,8 @@ func (m *Module) getPostTxHashes(txs []*coretypes.ResultTx, post types.Post) []t
 	return postTxs
 }
 
-// QuerySubspacePosts queries all the attachments for the given post at the provided height
-func (m *Module) queryPostAttachments(height int64, subspaceID uint64, postID uint64) ([]types.PostAttachment, error) {
+// QueryPostAttachments queries all the attachments for the given post at the provided height
+func (m *Module) QueryPostAttachments(height int64, subspaceID uint64, postID uint64) ([]types.PostAttachment, error) {
 	var attachments []types.PostAttachment
 
 	var nextKey []byte
