ad0f44efe9197b6c0a4672c33208ad6dd59d7012
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2023-08-23 10:56:10 -0500

    fix(lint): fixed deprecated type usage

--- utils/contracts.go
+++ utils/contracts.go
@@ -3,14 +3,14 @@ package utils
 import (
 	"encoding/json"
 
-	"github.com/CosmWasm/wasmd/x/wasm"
+	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
 
 	"github.com/desmos-labs/djuno/v2/types"
 )
 
 // IsMsgSendTip tries reading the given MsgExecuteContract as if
 // it's containing a send_tip message, and returns the inner message
-func IsMsgSendTip(msg *wasm.MsgExecuteContract) (*types.MsgSendTip, bool) {
+func IsMsgSendTip(msg *wasmtypes.MsgExecuteContract) (*types.MsgSendTip, bool) {
 	var msgTip types.TipMsg
 	err := json.Unmarshal(msg.Msg, &msgTip)
 	if err != nil {
