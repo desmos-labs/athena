36f532a151a00e2bf2e9e38e48c20f742bffb501
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2024-01-19 15:28:26 +0100

    feat: parse reactions events instead of messages

--- x/reactions/utils.go
+++ x/reactions/utils.go
@@ -3,6 +3,9 @@ package reactions
 import (
 	"context"
 
+	abci "github.com/cometbft/cometbft/abci/types"
+	"github.com/forbole/juno/v5/types/utils"
+
 	juno "github.com/forbole/juno/v5/types"
 
 	"github.com/forbole/juno/v5/node/remote"
@@ -23,6 +26,38 @@ func (m *Module) GetReactionID(tx *juno.Tx, index int) (uint32, error) {
 	}
 	return reactionstypes.ParseReactionID(reactionIDStr)
 }
+
+// GetReactionIDFromEvent returns the reaction ID from the given event
+func GetReactionIDFromEvent(event abci.Event) (uint32, error) {
+	reactionIDAttr, err := utils.FindAttributeByKey(event, reactionstypes.AttributeKeyReactionID)
+	if err != nil {
+		return 0, err
+	}
+
+	reactionID, err := reactionstypes.ParseReactionID(reactionIDAttr.Value)
+	if err != nil {
+		return 0, err
+	}
+
+	return reactionID, nil
+}
+
+// GetRegisteredReactionIDFromEvent returns the registered reaction ID from the given event
+func GetRegisteredReactionIDFromEvent(event abci.Event) (uint32, error) {
+	registeredReactionIDAttr, err := utils.FindAttributeByKey(event, reactionstypes.AttributeKeyRegisteredReactionID)
+	if err != nil {
+		return 0, err
+	}
+
+	registeredReactionID, err := reactionstypes.ParseRegisteredReactionID(registeredReactionIDAttr.Value)
+	if err != nil {
+		return 0, err
+	}
+
+	return registeredReactionID, nil
+}
+
+// -------------------------------------------------------------------------------------------------------------------
 
 func (m *Module) GetReaction(height int64, subspaceID uint64, postID uint64, reactionID uint32) (types.Reaction, error) {
 	res, err := m.client.Reaction(
