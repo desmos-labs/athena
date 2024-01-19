997e2cc481a5326c89f5a1460e045008e7a3d075
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2024-01-19 15:28:54 +0100

    chore: added missing event filtering

--- utils/events.go
+++ utils/events.go
@@ -5,11 +5,26 @@ import (
 	subspacestypes "github.com/desmos-labs/desmos/v6/x/subspaces/types"
 	juno "github.com/forbole/juno/v5/types"
 	"github.com/rs/zerolog/log"
+
+	"github.com/desmos-labs/athena/x/filters"
 )
+
+// GetSubspaceIDFromEvent returns the subspace ID from the given event
+func GetSubspaceIDFromEvent(event abci.Event) (uint64, error) {
+	attribute, err := juno.FindAttributeByKey(event, subspacestypes.AttributeKeySubspaceID)
+	if err != nil {
+		return 0, err
+	}
+	return subspacestypes.ParseSubspaceID(attribute.Value)
+}
 
 // ParseTxEvents parses the given events using the given parsers
 func ParseTxEvents(tx *juno.Tx, eventsParsers map[string]func(tx *juno.Tx, event abci.Event) error) error {
 	for _, event := range tx.Events {
+		if !filters.ShouldEventBeParsed(event) {
+			continue
+		}
+
 		parseEvent, canBeParsed := eventsParsers[event.Type]
 		if !canBeParsed {
 			continue
@@ -24,13 +39,4 @@ func ParseTxEvents(tx *juno.Tx, eventsParsers map[string]func(tx *juno.Tx, event
 	}
 
 	return nil
-}
-
-// GetSubspaceIDFromEvent returns the subspace ID from the given event
-func GetSubspaceIDFromEvent(event abci.Event) (uint64, error) {
-	attribute, err := juno.FindAttributeByKey(event, subspacestypes.AttributeKeySubspaceID)
-	if err != nil {
-		return 0, err
-	}
-	return subspacestypes.ParseSubspaceID(attribute.Value)
 }
