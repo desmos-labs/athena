ff20c6c6375236f8b5550c194bd13c4d803c6c27
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2024-02-02 10:25:21 +0100

    feat: added PSQL function to know if a profile is being followed by a user

--- database/schema/01-profiles.sql
+++ database/schema/01-profiles.sql
@@ -11,6 +11,22 @@ CREATE TABLE profile
 );
 CREATE INDEX profile_dtag_index ON profile (dtag);
 
+/**
+ * Function that allows to check if the current Hasura user is following a given profile.
+ */
+CREATE OR REPLACE FUNCTION is_user_following_profile(profile_row profile, hasura_session json)
+    RETURNS BOOLEAN AS
+$$
+SELECT EXISTS (SELECT 1
+               FROM user_relationship
+               WHERE user_relationship.subspace_id =
+                     CAST(COALESCE(hasura_session ->> 'x-hasura-selected-subspace-id', '0') AS BIGINT)
+                 AND user_relationship.counterparty_address = profile_row.address
+                 AND user_relationship.creator_address = hasura_session ->> 'x-hasura-user-address')
+$$ LANGUAGE sql STABLE;
+
+/* --------------------------------------------------------------------------------------------------------------- */
+
 CREATE TABLE dtag_transfer_requests
 (
     sender_address   TEXT   NOT NULL,
