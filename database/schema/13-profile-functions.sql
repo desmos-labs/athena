ff8e74de2223fa8971300a1e18433743542e80e0
Riccardo Montagnin <riccardo.montagnin@gmail.com>
2024-02-05 09:56:58 +0100

    feat: added field to know if a profile follows the Hasura user

--- database/schema/13-profile-functions.sql
+++ database/schema/13-profile-functions.sql
@@ -1,4 +1,18 @@
 /**
+ * Function that allows to check if the current Hasura user is followed by the given profile.
+ */
+CREATE OR REPLACE FUNCTION is_user_followed_by_profile(profile_row profile, hasura_session json)
+    RETURNS BOOLEAN AS
+$$
+SELECT EXISTS (SELECT 1
+               FROM user_relationship
+               WHERE user_relationship.subspace_id =
+                     CAST(COALESCE(hasura_session ->> 'x-hasura-selected-subspace-id', '0') AS BIGINT)
+                 AND user_relationship.creator_address = profile_row.address
+                 AND user_relationship.counterparty_address = hasura_session ->> 'x-hasura-user-address')
+$$ LANGUAGE sql STABLE;
+
+/**
  * Function that allows to check if the current Hasura user is following a given profile.
  */
 CREATE OR REPLACE FUNCTION is_user_following_profile(profile_row profile, hasura_session json)
